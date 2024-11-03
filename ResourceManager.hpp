937d9b6eabc4cba5fc1a08809351023a0898bacd
Kafel1997 <stanislaw.sufin@gmail.com>
2024-11-03 19:08:52 +0100

    wykonano dwa pierwsze zadania

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,21 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+        ResourceManager(){
+            rs_ = new Resource();
+        }
+
+        ResourceManager(const ResourceManager& rm): rs_{rm.rs_}{};
+
+        ~ResourceManager(){delete rs_;}
+
+        ResourceManager& operator=(ResourceManager& rm){
+            this->rs_=rm.rs_;
+            return *this;
+        }
+        double get(){return rs_->get();}
+    private:
+        Resource* rs_;
+
 };
